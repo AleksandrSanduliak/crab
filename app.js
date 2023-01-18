11df3eeea37719c72cfb812b039a042cfa418860
AleksandrSanduliak <lethalzmaneling@gmail.com>
2023-01-19 00:19:28 +0300

    get data from postgresql and assing to html localhost 5000

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const mysql = require('mysql2');
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root"
-});
+const { Client } = require('pg')
+const express = require('express')
+const PORT = 5000
+const app = express()
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-});
+
+const db = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'crabsystem',
+  password: 'root',
+  port: 5432,
+})
+const datas = {}
+let assinging = {}
+db.connect(err =>{
+  if(err) throw err +'err message'
+  else console.log('db connected')
+})
+db.query('SELECT * from cityinfo', (err, data) =>{
+  if(err) throw err + 'error query'
+  // console.log(data, err)
+  assinging = Object.assign(datas, data.rows)
+  db.end()
+})
+console.log(assinging)
+app.get('/', (req, res) => {
+  res.send(assinging)
+})
+app.listen(PORT, () => console.log('server run port `${PORT}`'))
