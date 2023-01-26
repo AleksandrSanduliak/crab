10eed972ac6350143d823c98ba2a1c0226824c1c
AleksandrSanduliak <lethalzmaneling@gmail.com>
2023-01-26 16:07:19 +0300

    edit vercel

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const { Client } = require('pg')
 const express = require('express')
+const path = require('path')
 const PORT = 5000
 const app = express()
-
-
+app.set('view engine', 'ejs')
 const db = new Client({
   user: 'postgres',
   host: 'localhost',
@@ -23,8 +23,12 @@ db.query('SELECT * from cityinfo', (err, data) =>{
   assinging = Object.assign(datas, data.rows)
   db.end()
 })
-console.log(assinging)
-app.get('/', (req, res) => {
-  res.send(assinging)
+// app.use('public/js', express.static(__dirname + '/js'));
+// app.use('/css', express.static(__dirname + '/css'));
+// app.use('/images', express.static(__dirname + '/images'));
+app.use('/assets', express.static('assets'))
+app.get('/', (req, res) =>{
+  // res.render('pages/index')
+  res.render('index')
 })
 app.listen(PORT, () => console.log('server run port `${PORT}`'))
