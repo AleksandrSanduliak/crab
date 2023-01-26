120d99f0aad32dce87b1c90da611acac922c43d1
AleksandrSanduliak <lethalzmaneling@gmail.com>
2023-01-26 23:53:05 +0300

    edit ejs

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const { Client } = require('pg')
 const express = require('express')
 const path = require('path')
-const PORT = 5000
 const app = express()
+const PORT = 5000
+
 app.set('view engine', 'ejs')
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 const db = new Client({
   user: 'postgres',
   host: 'localhost',
@@ -17,18 +20,16 @@ db.connect(err =>{
   if(err) throw err +'err message'
   else console.log('db connected')
 })
+
 db.query('SELECT * from cityinfo', (err, data) =>{
   if(err) throw err + 'error query'
   // console.log(data, err)
   assinging = Object.assign(datas, data.rows)
+
   db.end()
 })
-// app.use('public/js', express.static(__dirname + '/js'));
-// app.use('/css', express.static(__dirname + '/css'));
-// app.use('/images', express.static(__dirname + '/images'));
-app.use('/assets', express.static('assets'))
 app.get('/', (req, res) =>{
-  // res.render('pages/index')
-  res.render('index')
+  res.render('index', { params: assinging })
 })
-app.listen(PORT, () => console.log('server run port `${PORT}`'))
+
+app.listen(PORT, () => console.log('server run'))
