fb8f5a23f5d7db9885142f8c9d20ae6dad69166e
AleksandrSanduliak <lethalzmaneling@gmail.com>
2023-01-30 21:35:00 +0300

    vercel

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
-const app = require('express')();
-// const { v4 } = require('uuid');
-
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+const express = require("express");
+const router = express.Router();
+router.get("/", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "Get data has successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
 });
 
-app.get('/', (req, res) =>{
-    res.render('HELLO')
-  })
-
-module.exports = app;
+module.exports = router;
