2032383de5e0961882856b7b97f7db61518e45fd
AleksandrSanduliak <lethalzmaneling@gmail.com>
2023-01-30 20:42:45 +0300

    edit for vercel

--- public/js/header/burger.js
+++ public/js/header/burger.js
@@ -1,5 +1,6 @@
 import { disableScroll } from '../header/disable-scroll.js';
 import { enableScroll } from '../header/enable-scroll.js';
+// disable-scroll.js
 (function(){
   const burger = document?.querySelector('[data-burger]');
   const menu = document?.querySelector('[data-menu]');
