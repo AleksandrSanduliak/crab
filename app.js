426428dbb97d0380f6cd721ab639d825c833ef7a
AleksandrSanduliak <lethalzmaneling@gmail.com>
2023-02-20 00:36:54 +0300

    edit fn

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const {rejects} = require('assert')
 
 
 app.set('view engine', 'ejs')
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.get('/', (req, res) => {
