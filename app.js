bbb5fdf20544cea6becd31ac748865596fe91c7b
AleksandrSanduliak <lethalzmaneling@gmail.com>
2023-02-20 00:42:28 +0300

    edit fn

--- app.js
+++ app.js
@@ -5,18 +5,13 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-const index = require('./api/index')
 const {resolve} = require('path')
-const {rejects} = require('assert')
-
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render(path.join(__dirname + '/views/index.ejs'))
 })
-module.exports = app;
 app.listen(PORT, () => console.log(`server run on ${PORT}`))
