228dfe6f5137ef6d7b70d0178f9349b9ecacbc80
AleksandrSanduliak <lethalzmaneling@gmail.com>
2023-03-07 22:49:02 +0300

    refactoring app.js, edit firebase DB

--- app.js
+++ app.js
@@ -2,44 +2,50 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000
-const {
-  dbs
-} = require('./public/firebase/fire')
-const {
-  resolve
-} = require('path')
+const {dbs} = require('./public/firebase/fire')
+const {resolve} = require('path')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use('/public', express.static(path.join(__dirname, 'public', )));
 let products = []
-async function dataProducts() {
+let assinging = []
+let reviews = []
+
+async function data(array, collection) {
   try {
-    await dbs.collection('product').get().then(
+    await dbs.collection(collection).get().then(
       snapshot => {
         let docs = snapshot.docs
         for (let doc of docs) {
-          // products = Object.assign({}, doc.data())
-          products.push(doc.data())
+          array.push(doc.data())
         }
       })
-    return products
+    return array
   } catch (err) {
-    console.log(err + ' error promise firebase products')
+    console.log(err + ' error promise firebase' + collection)
   }
 }
 
-dataProducts()
-
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname + '/views/index.ejs'), {
-    // params: assinging,
-    prod: products,
-    // rev: reviews,
+  Promise.all([
+    data(products, 'product'),
+    data(assinging, 'cityinfo'),
+    data(reviews, 'reviews'),
+  ]).then(([promise1, promise2, promise3]) => {
+  console.log(promise1)
+  console.log(promise2)
+  console.log(promise3)
+  }).then(()=>{
+    res.render(path.join(__dirname + '/views/index.ejs'), {
+      params: assinging,
+      prod: products,
+      rev: reviews,
+    })
   })
 })
-app.get('/data', (req, res) => {
-  res.send(products)
-})
+// app.get('/data', (req, res) => {
+//   res.send(reviews[0])
+// })
 app.listen(PORT, () => console.log(`server run on ${PORT}`))
 module.exports = app
