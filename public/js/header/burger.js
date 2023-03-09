c6142b5356dc3d6b5e300c774ed8f21fd3778a32
AleksandrSanduliak <lethalzmaneling@gmail.com>
2023-03-09 11:48:08 +0300

    edit burger links

--- public/js/header/burger.js
+++ public/js/header/burger.js
@@ -1,5 +1,5 @@
-import { disableScroll } from '/public/js/header/disable-scroll.js';
-import { enableScroll } from '/public/js/header/enable-scroll.js';
+import { disableScroll } from './disable-scroll.js';
+import { enableScroll } from './enable-scroll.js';
 
 (function(){
   const burger = document?.querySelector('[data-burger]');
